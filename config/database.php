99cd76286deebd903ff10215aa50c16e8bd031ab
aomarabdelaziz <AbdelazizOmar1337@gmail.com>
2022-05-14 17:06:56 +0200

    Change Database

--- config/database.php
+++ config/database.php
@@ -64,7 +64,7 @@ return [
             ]) : [],
         ],
 
-        'pgsql1' => [
+        'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
             'host' => $DATABASE_URL['host'],
