0f2e98df8a999e77231b36b76cd8f97616f51c58
aomarabdelaziz <AbdelazizOmar1337@gmail.com>
2022-05-16 04:36:48 +0200

    Refractoring Code && Improve Performance

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,9 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class User extends Authenticatable
 {
@@ -33,6 +35,14 @@ class User extends Authenticatable
         'volunteer_type',
         'fcm_token'
     ];
+
+
+    public function nationalId() : Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => asset(Storage::url($value)),
+        );
+    }
 
     /**
      * The attributes that should be hidden for serialization.
