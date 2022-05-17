5cea69b58fcf8ecdac77b873f467879ff43581da
Abdelaziz Omar <abdelazizomar1337@gmail.com>
2022-05-17 15:26:40 +0200

    Edit Center Pending Role

--- app/Http/Controllers/Api/GetCentersController.php
+++ app/Http/Controllers/Api/GetCentersController.php
@@ -15,9 +15,10 @@ class GetCentersController extends Controller
 
     public function __invoke(Request $request)
     {
+
         $query = Center::query();
-        
-        $centers = $query->when(auth()->user()->role == 'user' , function ($query) {
+
+        $centers = $query->when(auth()->user()->role == 'user' || auth()->user()->role == 'center', function ($query) {
             return $query->where('status' , '=' , 'accepted');
         })->when(auth()->user()->role == 'admin' , function ($query) {
             return $query;
