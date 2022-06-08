f5d90e43f0c4af9132837d6995a893aad0dda56b
Abdelaziz Omar <abdelazizomar1337@gmail.com>
2022-06-08 21:12:48 +0200

    Fake User

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,11 +18,14 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'first_name' => $this->faker->name(),
+            'last_nane' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('admin'), // password
             'remember_token' => Str::random(10),
+            'phone' => '000000', // password
+            'role' => 'admin', // password
         ];
     }
 
